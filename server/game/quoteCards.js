2ba6b2ef96947d3972c1bdb5b518384c4ece207d
Kaitlin <kaitlinchoffmann@gmail.com>
2020-02-24 15:17:39 -0500

    Import bulma, style home and about with components

---
+++ server/game/quoteCards.js
@@ -0,0 +1,7 @@
+const quoteCards = [
+    "this class is awesome",
+    "come to shabot 200"
+];
+
+//part of commonjs
+module.exports = quoteCards; //this is a default export
--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+
+const quoteCards = require('./game/quoteCards'); // . is local
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app
+    .get('/', (req, res) => res.send('Hello World!'))
+    .get('/game/quoteCards', (req, res) => res.send(quoteCards));
+
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
