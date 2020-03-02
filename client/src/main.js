076a147eabf7b922d8c1f6a6ef08cb40e0996da3
Kaitlin <kaitlinchoffmann@gmail.com>
2020-03-02 15:17:42 -0500

    Install and Utilize FontAwesome

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bulma'; // a module within the bulma package
+import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.config.productionTip = false
 
