58b1fd3d00c0a79c45bde9fcc72202740ad90efb
Kaitlin <kaitlinchoffmann@gmail.com>
2020-03-09 15:13:33 -0400

    Managing quote cards

--- server/models/quoteCards.js
+++ server/models/quoteCards.js
@@ -1,7 +1,19 @@
 const quoteCards = [
-    "this class is awesome",
-    "come to shabot 200"
+    'Enjoy at least one sunset per day!',
+    'My bed is a magical place I suddenly remember everything I had to do',        'They say don’t try this at home… so I went to my friend’s home!',
+    'Wine + dinner = winner',
+    'Friday, my second favorite F word',
+    'Stomach: I will now demonstrate a blue whale’s mating call',
+    'I don’t care what people think of me. Mosquitos find me attractive!',
+    'If there would be an award for being lazy, I would send someone to pick it up for me.'
 ];
 
+function add(text) {
+    quoteCards.push(text);
+};
+
 //part of commonjs
-module.exports = quoteCards; //this is a default export
+module.exports = {
+    list: quoteCards, //this is a default export
+    add: add
+}
