57873e7714b3b7304a1471bc90e577b6d9bb4316
Kaitlin <kaitlinchoffmann@gmail.com>
2020-04-05 15:28:00 -0400

    Able to join game and submit captions

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -10,22 +10,21 @@ const router = express.Router();
 game.SubmitCaption("Corona Sucks", 0);
 
 router
-    .use('/quotecards', (req, res, next) =>  { //this is middleware
-        console.log({ method: req.method, body: req.body });
-        next(); //do not pass something in the parameter, will get an error
-    }) //next is a function
+    .get('/', (req, res) => res.send({
+        Players: game.Players, PictureDeck: game.PictureDeck, CurrentPicture: game.CurrentPicture,
+        CardsInPlay: game.CardsInPlay.map(x=> ({...x, PlayerId: 'unknown' }) ) 
+    }) )
+    .post('/join', (req, res)=> res.send(game.Join(req.body.userId)) )//next is a function
     .get('/quoteCards', (req, res) => res.send(quoteCards))
     .post('/quoteCards', (req, res) => {
         quoteCards.add(req.body.text);
         res.send(quoteCards.list[quoteCards.list.length-1]) //last item we just added
     })
-    .get('/', (req, res)=>res.send({
-        Players: game.Players, PictureDeck: game.PictureDeck, CurrentPicture: game.CurrentPicture,
-        CardsInPlay: game.CardsInPlay.map(x=> ({...x, PlayerId: 'unknown' }) )
-    }))
+    
     .post('/cardsInPlay', (req, res) => {
         const playerId = req.body.playerId;
         game.SubmitCaption(req.body.caption, playerId);
+        res.send({ success: true })
     })
 
 module.exports = router; //default export    
